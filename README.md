ccffbffe106cc7de8a377e24dd7219c3924dcb20
michy <mikedev.xda@gmail.com>
2023-07-25 00:57:46 +0200

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # web-development-resources
-Web Development resources such as Templates, components, websites for free resources (wallpapers, icons, illustrations, etc ...)
+Web Development resources such as **Templates**, **components**, websites for free **resources** (wallpapers, icons, illustrations, etc ...)
+
+
+The goal of this repository is to gather useful content for creative frontend development to create beautiful websites and web apps.
+
+For this work i'm going to use pure HTML, CSS and JavaScript
+
+![](./Templates/images/t1.png)
+
+## Legend
+- ✅: **Completed**
+- ⚠️: **Work in progress**
+- ⛔️: **Not started yet**
+
+## Table of contents
+- [Templates ⚠️](./Templates/README.md)
+- [Components ⛔️](./Components/README.md)
+- [Online Resources ⛔️](./OnlineResources/README.md)
---
+++ Templates/T7/script.js
@@ -0,0 +1,74 @@
+$(document).ready(function() {
+    let isDragging = false;
+    let startPosition = 0;
+    let endPosition = 0;
+    let translateX = 0;
+    let currentTranslateX = 0;
+    const cardWidth = $('.card').outerWidth(true);
+
+    $('.carousel-inner').on('mousedown touchstart', function(e) {
+      isDragging = true;
+      startPosition = e.pageX || e.originalEvent.touches[0].pageX;
+      currentTranslateX = translateX;
+      $(this).css('cursor', 'grabbing');
+    });
+
+    $('.carousel-inner').on('mousemove touchmove', function(e) {
+      if (!isDragging) return;
+      endPosition = e.pageX || e.originalEvent.touches[0].pageX;
+      const distance = endPosition - startPosition;
+      translateX = Math.min(0, Math.max(-cardWidth * ($('.card').length - 1), currentTranslateX + distance));
+      
+      // Nascondi la card quando raggiunge l'elemento a sinistra del carosello
+    //   if (translateX >= -600) {
+    //     $('.card').removeClass('hidden');
+    //   } else {
+    //     $('.card').addClass('hidden');
+    //   }
+    // Nascondi solo la card che tocca il limite sinistro del carosello
+    $('.card').each(function(index, card) {
+        const cardLeftPosition = index * cardWidth + translateX;
+        if (cardLeftPosition <= -700) {
+          $(card).addClass('hidden');
+        } else {
+          $(card).removeClass('hidden');
+        }
+      });
+      
+      $(this).css('transform', `translateX(${translateX}px)`);
+    });
+
+    $('.carousel-inner').on('mouseup touchend', function(e) {
+      isDragging = false;
+      $(this).css('cursor', 'grab');
+    });
+  });
+
+
+
+
+// $(document).ready(function() {
+//     let translateX = 0;
+//     const cardWidth = $('.card').outerWidth(true);
+//     const carouselWidth = $('.carousel').width();
+//     const maxTranslateX = 1000; // Imposta il limite desiderato in pixel
+
+//     $('.carousel-inner').on('wheel', function(e) {
+//       e.preventDefault();
+//       const delta = e.originalEvent.deltaY;
+//       translateX = Math.min(maxTranslateX, Math.max(carouselWidth - cardWidth * $('.card').length, translateX + delta));
+      
+//       // Nascondi solo la card che tocca il limite sinistro del carosello
+//       $('.card').each(function(index, card) {
+//         const cardLeftPosition = index * cardWidth + translateX;
+//         if (cardLeftPosition <= -600) {
+//           $(card).addClass('hidden');
+//         } else {
+//           $(card).removeClass('hidden');
+//         }
+//       });
+
+//       $(this).css('transform', `translateX(${translateX}px)`);
+//     });
+//   });
+
